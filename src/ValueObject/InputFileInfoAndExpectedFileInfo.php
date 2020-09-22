9c70226392c88b7336756a1f8119be22270ad49c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-09-22 20:30:45 +0700

    Bug fix: NoProtectedElementInFinalClassRule::isPropertyExistInTraits() should check namespaced class name (#2230)

---
+++ src/ValueObject/InputFileInfoAndExpectedFileInfo.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyTesting\ValueObject;
+
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class InputFileInfoAndExpectedFileInfo
+{
+    /**
+     * @var SmartFileInfo
+     */
+    private $inputFileInfo;
+
+    /**
+     * @var SmartFileInfo
+     */
+    private $expectedFileInfo;
+
+    public function __construct(SmartFileInfo $inputFileInfo, SmartFileInfo $expectedFileInfo)
+    {
+        $this->inputFileInfo = $inputFileInfo;
+        $this->expectedFileInfo = $expectedFileInfo;
+    }
+
+    public function getInputFileInfo(): SmartFileInfo
+    {
+        return $this->inputFileInfo;
+    }
+
+    public function getExpectedFileInfo(): SmartFileInfo
+    {
+        return $this->expectedFileInfo;
+    }
+
+    public function getExpectedFileInfoRealPath(): string
+    {
+        return $this->expectedFileInfo->getRealPath();
+    }
+}
