cc3f78487655764a2e9b2488731aabf4025628cd
Tomas Votruba <tomas.vot@gmail.com>
2020-10-26 11:38:48 +0100

    prepare release

---
+++ src/ValueObject/InputFileInfoAndExpectedFileInfo.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyTesting\ValueObject;
+
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class InputFileInfoAndExpectedFileInfo
+{
+    /**
+     * @var SmartFileInfo
+     */
+    private $inputFileInfo;
+
+    /**
+     * @var SmartFileInfo
+     */
+    private $expectedFileInfo;
+
+    public function __construct(SmartFileInfo $inputFileInfo, SmartFileInfo $expectedFileInfo)
+    {
+        $this->inputFileInfo = $inputFileInfo;
+        $this->expectedFileInfo = $expectedFileInfo;
+    }
+
+    public function getInputFileInfo(): SmartFileInfo
+    {
+        return $this->inputFileInfo;
+    }
+
+    public function getExpectedFileInfo(): SmartFileInfo
+    {
+        return $this->expectedFileInfo;
+    }
+
+    public function getExpectedFileContent(): string
+    {
+        return $this->expectedFileInfo->getContents();
+    }
+
+    public function getExpectedFileInfoRealPath(): string
+    {
+        return $this->expectedFileInfo->getRealPath();
+    }
+}
